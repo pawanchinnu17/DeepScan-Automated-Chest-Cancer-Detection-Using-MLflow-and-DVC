64436020c1b566dc8d8641ce9ebc98298e4f4996
pawanchinnu17 <pawanchinnu5@gmail.com>
2024-03-19 19:56:26 +0530

    added data ingestion

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("C:/pawan chinnu data science/Chest-Cancer-Detection/config/config.yaml")
+PARAMS_FILE_PATH = Path("C:/pawan chinnu data science/Chest-Cancer-Detection/params.yaml")
